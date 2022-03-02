0f4750b066f243a3c075a412e7c54c353432264e
Gilberto <Betho6990@gmail.com>
2022-03-01 23:23:00 -0600

    Or operator in an database consult using ORM

--- Sección 13 - Modelos Avanzados/biblioteca/applications/autor/managers.py
+++ Sección 13 - Modelos Avanzados/biblioteca/applications/autor/managers.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+# La Q nos ayuda a implementar sentencias de tipo or
+from django.db.models import Q
+
 class AutorManager(models.Manager):
     """ Managers para el modelo autor """ 
     
@@ -12,3 +15,12 @@ class AutorManager(models.Manager):
 
         
         return resultado
+    
+    
+    def buscar_autor1(self, kword):
+            
+            # Quiero que filtres a las instancias, cuyo kword sea similar al que estoy enviando o el apellido sea igual al kword que estoy enviando
+        resultados = self.filter(Q(nombre__icontains=kword) | Q(nombre__icontains=kword))
+
+        
+        return resultados
