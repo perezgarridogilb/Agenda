f944fe6723be179a216a39d9b4ddd0bfbff35711
Gilberto <Betho6990@gmail.com>
2022-03-03 20:53:51 -0600

    Django Ckeditor Configuration

--- Sección 15 - Blog personal y posicionamiento con Django/myblog/blog/applications/entrada/admin.py
+++ Sección 15 - Blog personal y posicionamiento con Django/myblog/blog/applications/entrada/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Category, Tag, Entry
+
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Entry)
--- Sección 15 - Blog personal y posicionamiento con Django/myblog/blog/blog/urls.py
+++ Sección 15 - Blog personal y posicionamiento con Django/myblog/blog/blog/urls.py
@@ -7,5 +7,7 @@ from django.urls import path, re_path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path('', include('applications.users.urls')),
-    re_path('', include('applications.home.urls')),
+    re_path('', include('applications.home.urls')), 
+    # urls para ckeditor
+    re_path(r'^ckeditor/', include('ckeditor_uploader.urls')),
 ]
