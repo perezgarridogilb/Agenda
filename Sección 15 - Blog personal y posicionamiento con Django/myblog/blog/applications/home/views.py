27c664b1db92a8ada1cb1fc9e002681b41407c67
Gilberto <Betho6990@gmail.com>
2022-03-05 18:04:33 -0600

    Home screen layout

--- Sección 15 - Blog personal y posicionamiento con Django/myblog/blog/applications/home/views.py
+++ Sección 15 - Blog personal y posicionamiento con Django/myblog/blog/applications/home/views.py
@@ -8,5 +8,17 @@ from django.views.generic import (
     TemplateView
 )
 
-class TestPlantilla(TemplateView):
-    template_name = "plantillas/register.html"
+# Apps entrada
+from applications.entrada.models import Entry
+
+class HomePageView(TemplateView):
+    template_name = "home/index.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super(HomePageView, self).get_context_data(**kwargs)
+        # Se quiere un contexto o palabra calve para la pantalla principal
+        # Contexto de portada
+        # Manager con el nombre entrada_en_portada
+        context["portada"] = Entry.objects.entrada_en_portada()
+        return context
+    
--- Sección 15 - Blog personal y posicionamiento con Django/myblog/blog/blog/urls.py
+++ Sección 15 - Blog personal y posicionamiento con Django/myblog/blog/blog/urls.py
@@ -3,6 +3,10 @@ Proyecto Curso Django
 """
 from django.contrib import admin
 from django.urls import path, re_path, include
+# Tiene la configuración de los media url
+from django.conf import settings
+# En caso de qué no esté subido, generemos a partir de un archivo static
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,4 +14,4 @@ urlpatterns = [
     re_path('', include('applications.home.urls')), 
     # urls para ckeditor
     re_path(r'^ckeditor/', include('ckeditor_uploader.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
