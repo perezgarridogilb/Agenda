2429d33a54afdf66bb393e4dce57acc8adbb0112
Gilberto <Betho6990@gmail.com>
2022-03-06 02:02:37 -0600

    Blog tab design

--- Sección 15 - Blog personal y posicionamiento con Django/myblog/blog/applications/entrada/views.py
+++ Sección 15 - Blog personal y posicionamiento con Django/myblog/blog/applications/entrada/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+# 
+from django.views.generic import (
+    ListView
+)
+
+class EntryListView(ListView):
+    # template_name = "TEMPLATE_NAME"
+    template_name = "entrada/lista.html"
+    context_object_name = 'entradas'
+    # Páginado de diez en diez a nuestra lista de entradas
+    paginate_by = 10
+    
+    def get_queryset(self):
+        return []
+    
+
