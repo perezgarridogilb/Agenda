f1b010de8380af9442e1ba290c65480b1acddf79
Gilberto <Betho6990@gmail.com>
2022-03-27 21:12:30 -0600

    Perform_created method in Viewset

--- Sección 17 - Servicios REST - Token Autentication, Generics ApiView y Viewsets/tiendadj/tienda/applications/producto/viewsets.py
+++ Sección 17 - Servicios REST - Token Autentication, Generics ApiView y Viewsets/tiendadj/tienda/applications/producto/viewsets.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
 
 from .models import Colors, Product
 # 
@@ -18,5 +19,19 @@ class ProductViewSet(viewsets.ModelViewSet):
     # Trabajar crud con
     serializer_class = ProductSerializerViewSet 
     queryset = Product.objects.all() 
-    pagination_class = PaginationSerializer
+    pagination_class = PaginationSerializer 
+    
+    # def create(self, request): 
+    #     print(request.data) 
+    #     return Response({'code': 'exitosos'}) 
+    
+    def perfom_create(self, serializer): 
+        serializer.save(
+            video="https://a.com/1/"
+        )
         
+    def list(self, request, *args, **kwargs): 
+        queryset = Product.objects.productos_por_user(self.request.user)
+        # 
+        serializer = self.get_serializer(queryset, many=True) 
+        return Response(serializer.data)    
