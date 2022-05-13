4d7f44546adda02607484ffbc26bda7083ad0eb0
Gilberto <Betho6990@gmail.com>
2022-05-13 18:02:14 -0500

    Customize Tables in the Django Manager

--- Sección 4 - Entorno de trabajo optimo con Django/empleado/applications/departamento/models.py
+++ Sección 4 - Entorno de trabajo optimo con Django/empleado/applications/departamento/models.py
@@ -1,3 +1,5 @@
+from enum import unique
+from tabnanny import verbose
 from unicodedata import name
 from django.db import models
 from django.forms import BooleanField
@@ -6,8 +8,15 @@ from django.forms import BooleanField
 class Departamento(models.Model):
     # blank=True|unique=True
     name = models.CharField('Nombre', max_length=55)
-    short_name = models.CharField('Nombre Corto', max_length=25)
+    short_name = models.CharField('Nombre Corto', max_length=25, unique=True)
     anulate = models.BooleanField('Anulado', default=False)
+    
+    class Meta:
+        verbose_name = 'Mi Departamento'
+        verbose_name_plural = 'Areas de la empresa'
+        ordering = ['-name']
+        unique_together = ('name', 'short_name')
+    
     
     def __str__(self):
         return str(self.id) + '-' + self.name + '-' + self.short_name
--- Sección 4 - Entorno de trabajo optimo con Django/empleado/applications/persona/models.py
+++ Sección 4 - Entorno de trabajo optimo con Django/empleado/applications/persona/models.py
@@ -2,7 +2,18 @@ from django.db import models
 # 
 from applications.departamento.models import Departamento
 
-# Create your models here.
+# Cada empleado va a tener un conjunto de habilidades
+
+class Habilidades(models.Model):
+    habilidad = models.CharField('Habilidad', max_length=55)
+    
+    class Meta:
+        verbose_name = 'Habilidad'
+        verbose_name_plural = 'Habilidades de la empresa'
+
+    def __str__(self):
+        return str(self.id) + '-' + self.habilidad # TODO
+    
 class Empleado(models.Model):
     """ Modelo para tabla empleado """
     
@@ -17,11 +28,19 @@ class Empleado(models.Model):
     # Administrador
     # Economista
     # Otro
+    
     first_name = models.CharField('Nombres', max_length=65)
     last_name = models.CharField('Apellidos',max_length=65)
     job = models.CharField('Trabajo', max_length=1, choices=JOB_CHOICES)
     departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
-    # img = models.ImageField(, upload_to=None, height_field=None, width_field=None, max_length=None)
+    avatar = models.ImageField(upload_to='empleado', blank=True, null=True)
+    habilidades = models.ManyToManyField(Habilidades)
+    
+    class Meta:
+        verbose_name = 'Mi Empleado'
+        verbose_name_plural = 'Empleados de la empresa'
+        ordering = ['-first_name', 'last_name']
+        unique_together = ('first_name', 'departamento')
     
     def __str__(self):
         return str(self.id) + '-' + self.first_name + '-' + self.last_name
