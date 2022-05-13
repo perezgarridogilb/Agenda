9a5d8e86767da0db49b7a5c7d49b3243f6daf1c8
Gilberto <Betho6990@gmail.com>
2022-05-12 23:58:03 -0500

    Related Models - Foreign Key

--- Sección 4 - Entorno de trabajo optimo con Django/empleado/applications/departamento/models.py
+++ Sección 4 - Entorno de trabajo optimo con Django/empleado/applications/departamento/models.py
@@ -1,3 +1,13 @@
+from unicodedata import name
 from django.db import models
+from django.forms import BooleanField
 
 # Create your models here.
+class Departamento(models.Model):
+    # blank=True|unique=True
+    name = models.CharField('Nombre', max_length=55)
+    short_name = models.CharField('Nombre Corto', max_length=25)
+    anulate = models.BooleanField('Anulado', default=False)
+    
+    def __str__(self):
+        return str(self.id) + '-' + self.name + '-' + self.short_name
